68565585f2cedad460fdde65be1775626c99ac05
domyuyi <yucacho2004@gmail.com>
2024-06-30 00:15:40 +0000

    Modificando el header y el basic

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import '/workspaces/project/src/css/Header.css'; 
+import { FaUser, FaShoppingCart, FaStore, FaGlobe, FaMicrochip } from 'react-icons/fa';
 
 const Header = () => {
     return (
         <header>
-            <nav className="navbar navbar-expand-lg bg-dark ">
+            <nav className="navbar navbar-expand-lg bg-dark">
                 <div className="container-fluid">
                     <Link className="navbar-brand text-white" to="/">
                         PC BUILDING WS
@@ -12,39 +13,39 @@ const Header = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-                        <ul className="navbar-nav mx-auto">
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav me-auto">
                             <li className="nav-item">
                                 <Link className="nav-link active text-white" to="/">
-                                    <i data-feather="cpu"></i> Inicio
+                                    <FaMicrochip /> Inicio
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link active text-white" to="/noticias">
-                                    <i data-feather="book-open"></i> Tienda
+                                    <FaStore /> Tienda
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link active text-white" to="/preguntas">
-                                    <i data-feather="globe"></i> Preguntas Frecuentes
+                                    <FaGlobe /> Preguntas Frecuentes
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link active text-white" to="/contactanos">
-                                    <i data-feather="globe"></i> Contáctanos
+                                    <span className="badge text-bg-light">
+                                        <FaShoppingCart /> ¡COMPRAR!
+                                    </span>
+                                </Link>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link active text-white" to="/iniciar-sesion">
+                                    <FaUser /> Iniciar sesión
                                 </Link>
                             </li>
                         </ul>
                     </div>
-                    <ul className="navbar-nav position-absolute end-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active text-white" to="/comprar">
-                                <span className="badge text-bg-light">
-                                    <i data-feather="shopping-cart"></i> ¡COMPRAR!
-                                </span>
-                            </Link>
-                        </li>
-                    </ul>
                 </div>
             </nav>
         </header>
--- src/layout/basic.js
+++ src/layout/basic.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./header";
+import Header from "./Header";
 import Footer from "./footer";
 
 
